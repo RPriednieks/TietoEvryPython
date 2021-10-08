510150b98f57050fec01ccd2da36db67689ad363
Rolands Priednieks <rolands.priednieks@tietoevry.com>
2021-10-08 17:23:25 +0300

    Second commit

--- Day_6_lists/lists.py
+++ Day_6_lists/lists.py
@@ -1,21 +1,21 @@
-# Exercise 1a and 2a
+# Exercise a1 and a2
 # Example for input: 22.3, 23.2, 44.5, 46.9, 55.7, 122.4, 66.9
 
-# entered_numbers = input("Enter several numbers (comma separated):")
-# if entered_numbers == "q":
-#     print("You decided to quit")
-# else:
-#     numbers_list = entered_numbers.split(", ")
-#     summa = 0
-#     for i in numbers_list:
-#         summa += float(i)
-#
-#     average = summa / len(numbers_list)
-#     saraksts = ", ".join(numbers_list)
-#     print(f"You did enter these numbers: {saraksts} and average of those is: {average}")
-#     print(numbers_list.sort())
+entered_numbers = input("Enter several numbers (comma separated):")
+if entered_numbers == "q":
+    print("You decided to quit")
+else:
+    numbers_list = entered_numbers.split(", ")
+    summa = 0
+    for i in numbers_list:
+        summa += float(i)
 
-# Exercise 3a
+    average = summa / len(numbers_list)
+    saraksts = ", ".join(numbers_list)
+    print(f"You did enter these numbers: {saraksts} and average of those is: {average}")
+    print(numbers_list.sort())
+
+# Exercise a3
 # Example for input: 22.3, 23.2, 44.5, 46.9, 55.7, 122.4, 66.9
 
 entered_numbers = input("Enter several numbers (comma separated): ")
@@ -35,6 +35,11 @@ TOP3 numbers are: {new_numbers_list[-1]}, {new_numbers_list[-2]} , {new_numbers_
 BOTTOM3 numbers are: {new_numbers_list[0]}, {new_numbers_list[1]} , {new_numbers_list[2]}
 Average of these numbers are: {average}
     ''')
+
+# Exercise 2
+
+first = input("Enter begining number:")
+last = input("Enter last number:")
 
 
 
