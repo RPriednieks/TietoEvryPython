3bf8577cfef898453e8423ecb0e2db209b876956
rolands <rolands.priednieks@gmail.com>
2021-10-11 18:55:34 +0300

    Second commit.

--- Day_7_functions/function.py
+++ Day_7_functions/function.py
@@ -24,7 +24,21 @@ def is_palindrome():
 
 is_palindrome()
 
+# Exercise 3
 
+def get_city_year(p0, perc, delta, p):
+    years = 0
+    original_p0 = p0
+    percentage_converted = float(perc/100)
+    while p0 < p:
+        if original_p0 > p0:
+            years = -1
+            break
+        p0 = p0 + (p0 * percentage_converted) + delta
+        years += 1
+    print(years)
 
-
-#input
+get_city_year(1000, 2, 50, 1200)
+get_city_year (1000, 2, -50, 5000)
+get_city_year (1500, 5, 100, 5000)
+get_city_year (1500000, 2.5, 10000, 2000000)
