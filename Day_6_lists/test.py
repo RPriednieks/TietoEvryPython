75ea801654d21fbc448c3445c760f544656bf60f
rolands <rolands.priednieks@gmail.com>
2021-10-12 16:00:22 +0300

    Class

--- Day_6_lists/test.py
+++ Day_6_lists/test.py
@@ -1,5 +1,15 @@
-num = 2
-num += 1
+import random
 
-print(num)
+class Person:
+    def __init__(self, first_name, last_name):
+        self.name = first_name
+        self.last = last_name
+
+    def talk(self):
+        print(f"Hey there {self.name}")
+
+john = Person("Johhny", "Priednieks")
+print(john.name)
+john.talk()
+
 
